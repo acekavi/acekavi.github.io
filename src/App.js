016fbd2168e0f09fa914132bd5eb1aa80fb63850
Avishka Kavinda <45445179+acekavi@users.noreply.github.com>
2021-02-12 15:44:18 +0530

    Before everything else

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hi this is a test react app on github
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App"> 
+      <h1>Welcome to my Portfolio</h1>
+      <h2>Site under construction</h2>
     </div>
-  );
+  )
 }
 
 export default App;
